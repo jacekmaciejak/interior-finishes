684133c02179e3a80126629924b6085db9c8fe1e
Jacek Maciejak <jacekmaciejak@poczta.fm>
2021-03-24 17:58:11 +0100

    Navbar & contact mobile view have been fixed

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import Title from '../Title/Title';
 import './ContactForm.css'
 
 function ContactForm() {
 
-    const inputs = document.querySelectorAll(".input");
+    const inputs = document.querySelectorAll(".form__input");
 
     function focusFunc() {
         let parent = this.parentNode;
@@ -26,79 +25,60 @@ function ContactForm() {
 
     return (
         <div className='contact__item'>
-            {/* <Title>Zapraszamy do kontaktu z nami!</Title> */}
             <div className="contact__container">
-                <span className="contact__big-circle"></span>
-                <img src="img/shape.png" className="contact__square" alt="" />
-                <div className="contact__wrapper">
-                    <div className="contact__info">
-                        <div className="info">
-                            <h3 className="title">Napisz do nas!</h3>
-                            <div className="input-container">
-                                <input type="text" name="name" className="input" />
+                {/* <span className="contact__big-circle"></span>
+                <img src="img/shape.png" className="contact__square" alt="" /> */}
+                <div className="contact__form">
+                    <span className="contact__circle one"></span>
+                    <span className="contact__circle two"></span>
+                    <h2 className="contact__title">Napisz do nas</h2>
+
+                    <form action="index.html" autocomplete="off">
+                        <div className="form__items">
+                            <div className="input__container">
+                                <input type="text" name="name" className="form__input" />
                                 <label for="">Imie i nazwisko</label>
                                 <span>Imie i nazwisko</span>
                             </div>
-                            <div className="input-container">
-                                <input type="email" name="email" className="input" />
+                            <div className="input__container">
+                                <input type="email" name="email" className="form__input" />
                                 <label for="">Email</label>
                                 <span>Email</span>
                             </div>
-                            <div className="input-container">
-                                <input type="tel" name="phone" className="input" />
+                            <div className="input__container">
+                                <input type="tel" name="phone" className="form__input" />
                                 <label for="">Telefon</label>
                                 <span>Telefon</span>
                             </div>
-                        </div>
-
-                        <div className="social__media">
-                            <p>Dołącz do naszej społeczności</p>
-                            <div className="social__icons">
-                                <a href="#">
-                                    <i className="fab fa-facebook-f"></i>
-                                </a>
-                                <a href="#">
-                                    <i className="fab fa-twitter"></i>
-                                </a>
-                                <a href="#">
-                                    <i className="fab fa-instagram"></i>
-                                </a>
-                                <a href="#">
-                                    <i className="fab fa-linkedin-in"></i>
-                                </a>
+                            <div className="social__media">
+                                <h2 className='contact__title'>Dołącz do naszej społeczności</h2>
+                                <div className="social__icons">
+                                    <a href="#">
+                                        <i className="fab fa-facebook-f"></i>
+                                    </a>
+                                    <a href="#">
+                                        <i className="fab fa-twitter"></i>
+                                    </a>
+                                    <a href="#">
+                                        <i className="fab fa-instagram"></i>
+                                    </a>
+                                    <a href="#">
+                                        <i className="fab fa-linkedin-in"></i>
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-
-                    <div className="contact__form">
-                        <span className="circle one"></span>
-                        <span className="circle two"></span>
-
-                        <form action="index.html" autocomplete="off">
-                            <h3 className="title">Napisz do nas!</h3>
-                            <div className="input-container">
-                                <input type="text" name="name" className="input" />
-                                <label for="">Imie i nazwisko</label>
-                                <span>Imie i nazwisko</span>
-                            </div>
-                            <div className="input-container">
-                                <input type="email" name="email" className="input" />
-                                <label for="">Email</label>
-                                <span>Email</span>
-                            </div>
-                            <div className="input-container">
-                                <input type="tel" name="phone" className="input" />
-                                <label for="">Telefon</label>
-                                <span>Telefon</span>
-                            </div>
+                        <div className="form__message">
                             <div className="input__container textarea">
-                                <textarea name="message" className="input"></textarea>
+                                <textarea name="message" className="form__input"></textarea>
                                 <label for="">Wiadomość</label>
                                 <span>Wiadomość</span>
                             </div>
-                            <input type="submit" value="Send" className="btn" />
-                        </form>
-                    </div>
+                            <input type="submit" value="Wyślij" className="form__btn" />
+                        </div>
+
+
+                    </form>
                 </div>
             </div>
         </div>
