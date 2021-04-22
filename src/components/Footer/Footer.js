e5f741b62c55afd828dc8c9cce8dcd2d38a96864
Jacek Maciejak <jacekmaciejak@poczta.fm>
2021-04-22 19:12:42 +0200

    Footer section has been changed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,8 +5,8 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
-        <div className='footer-container'>
-            <section className='footer-subscription'>
+        <div className='footer__container'>
+            {/* <section className='footer-subscription'>
                 <p className='footer-subscription-heading'>
                     Zapraszamy do zapisania się do newslettera
         </p>
@@ -24,54 +24,26 @@ function Footer() {
                         <Button buttonStyle='btn--outline'>Subskrybuj</Button>
                     </form>
                 </div>
-            </section>
-            <div class='footer-links'>
-                <div className='footer-link-wrapper'>
-                    <div class='footer-link-items'>
-                        <h2>About Us</h2>
-                        <Link to='/sign-up'>How it works</Link>
-                        <Link to='/'>Testimonials</Link>
-                        <Link to='/'>Careers</Link>
-                        <Link to='/'>Investors</Link>
-                        <Link to='/'>Terms of Service</Link>
+            </section> */}
+            <div class='footer__links'>
+                <div className='footer__link__wrapper'>
+                    <div class='footer__link__items'>
+                        <h2>O nas</h2>
+                        <Link to='/sign-up'>Jak działamy?</Link>
+                        <Link to='/'>Referencje</Link>
+                        <Link to='/'>Kariera</Link>
+                        <Link to='/'>Dla inwestora</Link>
                     </div>
-                    <div class='footer-link-items'>
-                        <h2>Contact Us</h2>
-                        <Link to='/'>Contact</Link>
-                        <Link to='/'>Support</Link>
-                        <Link to='/'>Destinations</Link>
-                        <Link to='/'>Sponsorships</Link>
-                    </div>
-                </div>
-                <div className='footer-link-wrapper'>
-                    <div class='footer-link-items'>
-                        <h2>Videos</h2>
-                        <Link to='/'>Submit Video</Link>
-                        <Link to='/'>Ambassadors</Link>
-                        <Link to='/'>Agency</Link>
-                        <Link to='/'>Influencer</Link>
-                    </div>
-                    <div class='footer-link-items'>
-                        <h2>Social Media</h2>
+                    <div class='footer__link__items'>
+                        <h2>Media</h2>
                         <Link to='/'>Instagram</Link>
                         <Link to='/'>Facebook</Link>
                         <Link to='/'>Youtube</Link>
                         <Link to='/'>Twitter</Link>
                     </div>
-                </div>
-            </div>
-            <section class='social-media'>
-                <div class='social-media-wrap'>
-                    <div class='footer-logo'>
-                        <Link to='/' className='social-logo'>
-
-                            Jacek Maciejak
-                            <i class="fas fa-tools"></i>
-                        </Link>
-                    </div>
-                    <div class='social-icons'>
+                    <div class='footer__icons'>
                         <Link
-                            class='social-icon-link facebook'
+                            class='social__icon__link facebook'
                             to='/'
                             target='_blank'
                             aria-label='Facebook'
@@ -79,7 +51,7 @@ function Footer() {
                             <i class='fab fa-facebook-f' />
                         </Link>
                         <Link
-                            class='social-icon-link instagram'
+                            class='social__icon__link instagram'
                             to='/'
                             target='_blank'
                             aria-label='Instagram'
@@ -87,7 +59,7 @@ function Footer() {
                             <i class='fab fa-instagram' />
                         </Link>
                         <Link
-                            class='social-icon-link youtube'
+                            class='social__icon__link youtube'
                             to='/'
                             target='_blank'
                             aria-label='Youtube'
@@ -95,7 +67,7 @@ function Footer() {
                             <i class='fab fa-youtube' />
                         </Link>
                         <Link
-                            class='social-icon-link twitter'
+                            class='social__icon__link twitter'
                             to='/'
                             target='_blank'
                             aria-label='Twitter'
@@ -103,7 +75,7 @@ function Footer() {
                             <i class='fab fa-twitter' />
                         </Link>
                         <Link
-                            class='social-icon-link twitter'
+                            class='social__icon__link twitter'
                             to='/'
                             target='_blank'
                             aria-label='LinkedIn'
@@ -111,8 +83,34 @@ function Footer() {
                             <i class='fab fa-linkedin' />
                         </Link>
                     </div>
+                    {/* <div class='footer-link-items'>
+                        <h2>Videos</h2>
+                        <Link to='/'>Submit Video</Link>
+                        <Link to='/'>Ambassadors</Link>
+                        <Link to='/'>Agency</Link>
+                        <Link to='/'>Influencer</Link>
+                    </div> */}
+                    {/* <div class='footer-link-items'>
+                        <h2>Kontakt</h2>
+                        <Link to='/'>Contact</Link>
+                        <Link to='/'>Support</Link>
+                        <Link to='/'>Destinations</Link>
+                        <Link to='/'>Sponsorships</Link>
+                    </div> */}
                 </div>
-            </section>
+            </div>
+            {/* <section class='social__media'>
+                <div class='social__media__wrap'>
+                    <div class='footer-logo'>
+                        <Link to='/' className='social-logo'>
+
+                            Jacek Maciejak
+                            <i class="fas fa-tools"></i>
+                        </Link>
+                    </div>
+
+                </div>
+            </section> */}
         </div>
     );
 }
