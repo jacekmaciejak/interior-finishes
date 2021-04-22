0490f3b99e1dfcdd3f61fc597f13705154525024
Jacek Maciejak <jacekmaciejak@poczta.fm>
2021-04-22 19:25:39 +0200

    Navbar issues have been fixed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import Button from '../Button/Button';
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
@@ -83,7 +82,7 @@ function Navbar() {
 
                         </li>
                     </ul>
-                    {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+                    {/* {button} */}
                 </div>
             </nav>
         </>
