7a056fea4941f9fd99917cd3ec0ccf58a064a8a3
Jacek Maciejak <jacekmaciejak@poczta.fm>
2021-06-09 17:59:15 +0200

    New contact form has been added

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -4,90 +4,49 @@ import './ContactForm.css'
 
 const ContactForm = () => {
 
-    const inputs = document.querySelectorAll(".form__input");
+    // const inputs = document.querySelectorAll(".form__input");
 
-    function focusFunc() {
-        let parent = this.parentNode;
-        parent.classList.add("focus");
-
-    }
-
-    function blurFunc() {
-        let parent = this.parentNode;
-        if (this.value === "") {
-            parent.classList.remove("focus");
-        }
-
-    }
-
-    inputs.forEach((input) => {
-        input.addEventListener("focus", focusFunc);
-        input.addEventListener("blur", blurFunc);
-    });
-
-    // const xxx = () => {
-    //     return (inputs.forEach((input) => {
-    //         input.addEventListener("focus", focusFunc);
-    //         input.addEventListener("blur", blurFunc);
-    //     }))
-
+    // function focusFunc() {
+    //     let parent = this.parentNode;
+    //     parent.classList.add("focus");
 
     // }
 
+    // function blurFunc() {
+    //     let parent = this.parentNode;
+    //     if (this.value === "") {
+    //         parent.classList.remove("focus");
+    //     }
+
+    // }
+
+    // inputs.forEach((input) => {
+    //     input.addEventListener("focus", focusFunc);
+    //     input.addEventListener("blur", blurFunc);
+    // });
+
 
     return (
-        <div className='contact__item'>
-            <div className="contact__container">
-                <div className="contact__form">
-                    <form className='contact__wrapper' action="index.html" autocomplete="off">
-                        <div className="form__items">
-                            <div className="input__container">
-                                <input type="text" name="name" className="form__input" />
-                                <label for="">Imie i nazwisko</label>
-                                <span>Imie i nazwisko</span>
-                            </div>
-                            <div className="input__container">
-                                <input type="email" name="email" className="form__input" />
-                                <label for="">Email</label>
-                                <span>Email</span>
-                            </div>
-                            <div className="input__container">
-                                <input type="tel" name="phone" className="form__input" />
-                                <label for="">Telefon</label>
-                                <span>Telefon</span>
-                            </div>
-                            <div className="social__media">
-                                <h2 className='contact__title'>Dołącz do naszej społeczności</h2>
-                                <div className="social__icons">
-                                    <a href="#">
-                                        <i className="fab fa-facebook-f"></i>
-                                    </a>
-                                    <a href="#">
-                                        <i className="fab fa-twitter"></i>
-                                    </a>
-                                    <a href="#">
-                                        <i className="fab fa-instagram"></i>
-                                    </a>
-                                    <a href="#">
-                                        <i className="fab fa-linkedin-in"></i>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="form__message">
-                            <div className="input__container textarea">
-                                <textarea name="message" className="form__input"></textarea>
-                                <label for="">Wiadomość</label>
-                                <span>Wiadomość</span>
-                            </div>
-                            <Button buttonStyle='btn--center'>Wyślij</Button>
-                        </div>
-
-
-                    </form>
+        <section className='contact__container'>
+            <form>
+                <h2>zapraszamy do kontaktu</h2>
+                <div className="inputBox">
+                    <input type="text" name='' required='required' />
+                    <span>imie i nazwisko</span>
                 </div>
-            </div>
-        </div>
+                <div className="inputBox">
+                    <input type="text" name='' required='required' />
+                    <span>email</span>
+                </div>
+                <div className="inputBox">
+                    <textarea cols="30" rows="10" required='required'></textarea>
+                    <span>napisz wiadomosc</span>
+                </div>
+                <div className="inputBox">
+                    <input type="submit" value='Wyślij' />
+                </div>
+            </form>
+        </section>
 
 
     )
